8e90427f6f8ba295a5962c1016cbca1a2b96a624
vmnavarro94 <vm.navarro94@gmail.com>
2024-12-03 10:10:58 -0600

    cosmere art

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -23,9 +23,14 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include "peripheral_status.h"
 
-LV_IMG_DECLARE(balloon);
-LV_IMG_DECLARE(mountain);
 LV_IMG_DECLARE(cosmere_bit);
+LV_IMG_DECLARE(kr_bit);
+
+const lv_img_dsc_t *anim_imgs[] = {
+    &cosmere_bit,
+    &kr_bit,
+};
+
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -115,12 +120,14 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    lv_obj_t *art = lv_img_create(widget->obj);
-    //bool random = sys_rand32_get() & 1;
-    //lv_img_set_src(art, random ? &balloon : &mountain);
-    lv_img_set_src(art, &cosmere_bit); //new line
+    lv_obj_t * art = lv_animimg_create(widget->obj);
+    lv_obj_center(art);
+    lv_animimg_set_src(art, (const void **) anim_imgs, 2);
+    lv_animimg_set_duration(art, CONFIG_CUSTOM_ANIMATION_SPEED);
+    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
+    lv_animimg_start(art);
+    
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
-
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
     widget_peripheral_status_init();
